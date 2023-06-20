89b01d43a5b6eb7f923d9c21c2cb5fd1fda67013
vicarb <vcardenasb@protonmail.com>
2023-06-19 21:17:06 -0400

    updated layout

--- components/Reader/Reader.tsx
+++ components/Reader/Reader.tsx
@@ -46,58 +46,59 @@ const Reader = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <div className="p-6 bg-white shadow-md rounded-md w-2/3">
-        <div className="relative h-64 overflow-hidden">
-          {isReading ? (
-            <div className="absolute inset-0 flex items-center justify-center text-3xl font-bold text-gray-700">
-              {currentWord}
-            </div>
-          ) : (
-            <textarea 
-              className="w-full h-full p-2 mb-4 border-gray-300 rounded-md" 
-              value={text} 
-              onChange={(e) => setText(e.target.value)}
-              placeholder="Type or paste your text here"
-            />
-          )}
-        </div>
-        { !isReading && (
-          <div className="mt-4 flex space-x-4">
-            <button 
-              className="px-4 py-2 font-semibold text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700"
-              onClick={handleStart}
-            >
-              Start
-            </button>
-            <button 
-              className="px-4 py-2 font-semibold text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700"
-              onClick={handleClear} 
-            >
-              Clear
-            </button>
-            <div className="self-center">
-              <label htmlFor="wpm">WPM: </label>
-              <input 
-                id="wpm" 
-                type="number" 
-                min="10"
-                max="1000"
-                value={wpm} 
-                onChange={(e) => setWpm(Number(e.target.value))} 
-              />
-            </div>
+    <h1 className="text-2xl font-bold mb-4">Speed Reading Application</h1>
+    <div className="p-6 bg-white shadow-md rounded-md w-2/3">
+      <div className="relative h-64 overflow-hidden">
+        {isReading ? (
+          <div className="absolute inset-0 flex items-center justify-center text-3xl font-bold text-gray-700">
+            {currentWord}
           </div>
-        )}
-        { isReading && (
-          <button 
-            className="mt-4 px-4 py-2 font-semibold text-white bg-red-600 rounded-lg shadow-md hover:bg-red-700"
-            onClick={handleStop}
-          >
-            Stop
-          </button>
+        ) : (
+          <textarea 
+            className="w-full h-full p-2 mb-4 border-gray-300 rounded-md" 
+            value={text} 
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Type or paste your text here"
+          />
         )}
       </div>
+      { !isReading && (
+        <div className="mt-4 flex space-x-4">
+          <button 
+            className="px-4 py-2 font-semibold text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700"
+            onClick={handleStart}
+          >
+            Start
+          </button>
+          <button 
+            className="px-4 py-2 font-semibold text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700"
+            onClick={handleClear} 
+          >
+            Clear
+          </button>
+          <div className="self-center">
+            <label htmlFor="wpm">WPM: </label>
+            <input 
+              id="wpm" 
+              type="number" 
+              min="10"
+              max="1000"
+              value={wpm} 
+              onChange={(e) => setWpm(Number(e.target.value))} 
+            />
+          </div>
+        </div>
+      )}
+      { isReading && (
+        <button 
+          className="mt-4 px-4 py-2 font-semibold text-white bg-red-600 rounded-lg shadow-md hover:bg-red-700"
+          onClick={handleStop}
+        >
+          Stop
+        </button>
+      )}
     </div>
+  </div>
   );
 }
 
